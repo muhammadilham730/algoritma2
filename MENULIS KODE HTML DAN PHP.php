f1e34f0eb0495c793255d32541cb8bc1874aae8c
muhammadilham730 <mi8190044@gmail.com>
2024-08-27 12:31:28 +0700

    menulis kode html dan php

---
+++ MENULIS KODE HTML DAN PHP.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+    <title><?php echo "belajar php" ?></title>
+</head>
+<body>
+   <?php
+ echo "kita sedang belajar php<br>";
+ echo "<p> belajar php hingga jadi master</p>";
+ echo "<p><strong>by si bray ilham</p>";
+ ?>
+</body>
+</html>

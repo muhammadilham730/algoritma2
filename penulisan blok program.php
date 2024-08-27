d468d5790bbc68bee710287d976134470300a749
muhammadilham730 <mi8190044@gmail.com>
2024-08-27 14:09:06 +0700

    penulisan blok program

---
+++ penulisan blok program.php
@@ -0,0 +1,10 @@
+<?php
+$umur=20;{
+    echo "<p>kamu  sudah dewasa";
+    echo "<pselamat datang";
+    echo ",p>kamu boleh main game";
+
+    echo "<br><br><br><strong>by muhammad ilham</strong>";
+
+
+}

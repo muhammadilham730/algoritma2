7246b3ed700e59acbaa914423523d333bd4540c5
muhammadilham730 <mi8190044@gmail.com>
2024-08-27 13:13:46 +0700

    penulisan statement dan ekspresi

---
+++ penulisan statmen dan ekspresi.php
@@ -0,0 +1,15 @@
+<?php
+echo "ini statement 1";
+echo "ini statement 2";
+//deklarasi membuat variabel
+$b=8;
+$c=9;
+//ekspresi
+$a1 = $b + $c;
+$a2 = $b + $c;
+//cetak ekspresi
+echo "<p> ini ekspresi -> $a1 =$b + $c";
+echo "<p> ini ekspresi -> $a1 =$b + $c";
+echo "<p> yaitu : .$a1";
+echo "<p> yaitu : .$a2";
+echo "<p><p>";

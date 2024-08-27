69d9c3492a6d8132c01797a322d8f5e550ec0613
muhammadilham730 <mi8190044@gmail.com>
2024-08-27 12:42:28 +0700

    menulis php dalam html

---
+++ MENULIS PHP DALAM HTML.php
@@ -0,0 +1,11 @@
+<?php
+echo "<html>";
+echo "<head>";
+echo "<title>judul web<title>";
+echo "</head>";
+echo "<body>";
+echo "<h1>selamat datang</h1>";
+echo "di smk 1 karang baru aceh tamiang";
+echo "</body>";
+echo "</html>";
+echo "<br><br><br><strong>by muhammad ilham!";

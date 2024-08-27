3c23615460fc11336ecfd3c1b23ccfcab670d17d
muhammadilham730 <mi8190044@gmail.com>
2024-08-27 13:40:52 +0700

    penulisan komentar

---
+++ penulisan komentar php.php
@@ -0,0 +1,13 @@
+<?php
+
+//ini adalah komentar
+echo "hello nama saya muhammad ilham";
+
+/*
+ini adalah komentar 
+yang lebih dari satu
+baris
+*/
+
+
+echo "<br><br><br><strong>by muhammad ilham</strong>";

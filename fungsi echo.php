07cc060c6f918e2006bc55c327a5860b257ba612
muhammadilham730 <mi8190044@gmail.com>
2024-08-28 09:01:48 +0700

    fungsi echo

---
+++ fungsi echo.php
@@ -0,0 +1,8 @@
+<?php
+echo "<h2>Belajar PHP itu mudah!</h2>";
+echo("Hello muhammad ilham!<br>");
+echo "Disedang belajar PHP!<br>";
+echo "Ini ", "teks ", "yang ", "dibuat ", "terpisah.";
+
+echo "<br><br><br><strong>By muhammad ilham </strong>";
+?>

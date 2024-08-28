49cfbe0e017ede215b9693ff6ea331d5aacd410a
muhammadilham730 <mi8190044@gmail.com>
2024-08-28 10:15:53 +0700

    tipe data

---
+++ tipe data.php
@@ -0,0 +1,26 @@
+<?php
+
+// tipe data char (karakter)
+$jenis_kelamin = 'L';
+
+// tipe data string (teks)
+$nama_lengkap = "rian";
+
+// tipe data integer
+$umur = 16;
+
+// tipe data float
+$berat = 70;
+
+// tipe data float
+$tinggi = 175;
+
+// tipe data boolean
+$menikah = false;
+
+echo "Nama: $nama_lengkap<br>";
+echo "Jenis Kelamin: $jenis_kelamin<br>";
+echo "Umur: $umur tahun<br>";
+echo "berat badan: $berat kg<br>";
+echo "tinggi badan: $tinggi cm<br>";
+echo "menikah: $menikah";
